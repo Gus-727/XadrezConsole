7a07457343c4a0bfe1a0f3b2fbc2e3c370d7d3f6
Gus-727 <165914889+Gus-727@users.noreply.github.com>
2026-06-17 09:52:26 -0300

    Criando a classe Posicao

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using XadrezConsole.Tabuleiro;
+
+namespace XadrezConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao pos = new Posicao(3, 4);
+            Console.WriteLine(pos);
+        }
+    }
+}
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrezConsole.Tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
